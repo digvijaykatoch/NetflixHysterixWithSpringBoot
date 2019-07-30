822d1e85b0460657b9e457a87bfbef31924351a3
Dinesh <dinesh.gopalj@gmail.com>
2019-07-30 00:07:35 -0500

    Base Version
    Change-Id: Ie0614e7e61656539aac40750b7deb80fe47d558c

---
+++ microservices-java/src/main/java/com/microservices/module/microservicesjava/MicroservicesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.module.microservicesjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesJavaApplication.class, args);
+	}
+
+}
